ca5941578210f315334a699ea0528c02654e967c
newesish <hugo@tbf.mx>
2014-05-20 15:26:58 -0500

    pagina principal de tbf con forma de contacto + creta

---
+++ app/controllers/contacto_controller.rb
@@ -0,0 +1,29 @@
+class ContactoController < ApplicationController
+
+  def index
+  end
+
+  def sender
+  	@datos = contacto_params
+    if 	Mailer.sended(@datos).deliver 
+        redirect_to contacto_url, notice: 'Mensaje enviado exitosamente!'
+      else
+        redirect_to :action => "fail"
+    end      
+    #puts  @datos 	
+  end
+
+  def fail
+    puts "fallo";
+  end
+
+  def success
+  end
+
+  private
+
+  def contacto_params
+    params.permit(:nombre, :email, :ciudad, :pais, :area_web, :area_realidad, :area_modulos, :area_video, :mensajes)
+  end
+
+end
