81bd855ac0b0fa750f7b7c9675f1f417b0e1bf92
newesish <hugo@tbf.mx>
2014-06-30 17:59:41 -0500

    se cambio el mail de la forma de contacto de tbf de Hugo a Mike

--- app/mailers/mailer.rb
+++ app/mailers/mailer.rb
@@ -46,7 +46,7 @@ class Mailer < ActionMailer::Base
     @greeting = "Hi"
     @datos = datos
 
-    mail(:to => "hugo@tbf.mx", :subject => "Nuevo Mensaje de Contacto", :from => "\"Conserje\" <info@tbf.mx>", :reply_to =>"conserje@tbf.com")
+    mail(:to => "miguel@tbf.mx", :subject => "Nuevo Mensaje de Contacto", :from => "\"Conserje\" <info@tbf.mx>", :reply_to =>"conserje@tbf.com")
     #mail to: "hugo@tbf.mx"
   end
   
